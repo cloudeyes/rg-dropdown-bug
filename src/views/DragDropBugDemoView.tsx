3568c4fdce4c9314eaa980c0b0d311a4196852e0
Jopseh Kim <cloudeyes@gmail.com>
2024-12-16 11:09:51 +0900

    Add reset button

--- src/views/DragDropBugDemoView.tsx
+++ src/views/DragDropBugDemoView.tsx
@@ -1,50 +1,111 @@
+import { useRef } from "react";
+import RealGrid, {
+  GridView as GridViewNew,
+  LocalDataProvider as LocalDataProviderNew,
+} from "realgrid";
+import { gridVersion, GridView, LocalDataProvider } from "realgrid-2.7.2";
 import ExampleGrid from "./_components/ExampleGrid";
 import { columns, fields, rows } from "./realgrid-data";
-import { GridView, LocalDataProvider } from 'realgrid-2.7.2';
-import { GridView as GridViewNew, LocalDataProvider as LocalDataProviderNew } from 'realgrid';
 
 const getGridView = (container: HTMLDivElement) => {
   const gridView = new GridView(container);
   const provider = new LocalDataProvider(false);
   gridView.setDataSource(provider);
+  console.log("gridView:", gridVersion);
   return gridView;
 };
 
 const getGridViewBuggy = (container: HTMLDivElement) => {
-  const gridView = new GridViewNew(container) as unknown as GridView;
-  const provider = new LocalDataProviderNew(false) as unknown as LocalDataProvider;
+  const rawGridView = new GridViewNew(container);
+  const gridView = rawGridView as unknown as GridView;
+  const provider = new LocalDataProviderNew(
+    false
+  ) as unknown as LocalDataProvider;
+  console.log("gridViewBuggy:", RealGrid.getVersion());
   gridView.setDataSource(provider);
   return gridView;
 };
 
 export default function DragDropBugDemoView() {
-  return <>
+  const targetGrid1 = useRef<GridView>();
+  const targetGrid2 = useRef<GridView>();
+
+  return (
+    <>
       <div className="header flex flex-row gap-2 p-2 py-1">
-        <h1 className="text-center text-lg font-semibold">RealGrid Dropdown Bug : Drop Items to the Empty Grid</h1>
+        <h1 className="text-center text-lg font-semibold">
+          RealGrid Dropdown Bug : Drop Items to the Empty Grid
+        </h1>
       </div>
-      <h2 className="text-lg font-semibold px-2 bg-green-50">v2.7.x : Works</h2>
+      <h2 className="flex items-center text-lg font-semibold px-2 py-1 bg-green-50">
+        v2.7.x : Works
+        <button
+          className="text-sm text-black font-normal ml-5 px-2 py-1 rounded-md border bg-gray-100 hover:bg-gray-200"
+          onClick={() => {
+            console.log("targetGrid1:", targetGrid1);
+            targetGrid1.current?.getDataSource().clearRows();
+          }}
+        >
+          Reset
+        </button>
+      </h2>
       <div className="vertical-split flex flex-row flex-1 h-full">
         <div className="left flex flex-col bg-blue-50 min-w-72">
           <h2 className="px-2 bg-blue-100">Drag Source</h2>
-          <ExampleGrid getGridView={getGridView} movable hideRowIndicator rows={rows} columns={columns} fields={fields}/>
+          <ExampleGrid
+            getGridView={getGridView}
+            dragSource
+            hideRowIndicator
+            rows={rows}
+            columns={columns}
+            fields={fields}
+          />
         </div>
         <div className="split-line w-1 bg-gray-300"></div>
         <div className="right flex flex-1 flex-col bg-red-50">
           <h2 className="px-2 bg-red-100">Drop Target</h2>
-          <ExampleGrid getGridView={getGridView} rows={[]} columns={columns} fields={fields}/>
+          <ExampleGrid
+            getGridView={getGridView}
+            rows={[]}
+            columns={columns}
+            fields={fields}
+            onInitGrid={(_) => (targetGrid1.current = _)}
+          />
         </div>
       </div>
-      <h2 className="text-lg font-semibold px-2 bg-red-50 text-red-700">v2.8.x : Doesn't Work (Bug)</h2>
+      <h2 className="flex items-center text-lg font-semibold px-2 py-1 text-red-700 bg-red-50">
+        v2.8.x : Didn't Work
+        <button
+          className="text-sm text-black font-normal ml-5 px-2 py-1 rounded-md border bg-gray-100 hover:bg-gray-200"
+          onClick={() => targetGrid2.current?.getDataSource().clearRows()}
+        >
+          Reset
+        </button>
+      </h2>
       <div className="vertical-split flex flex-row flex-1 h-full">
         <div className="left flex flex-col bg-blue-50 min-w-72">
           <h2 className="px-2 bg-blue-100">Drag Source</h2>
-          <ExampleGrid getGridView={getGridViewBuggy} movable hideRowIndicator rows={rows} columns={columns} fields={fields}/>
+          <ExampleGrid
+            getGridView={getGridViewBuggy}
+            dragSource
+            hideRowIndicator
+            rows={rows}
+            columns={columns}
+            fields={fields}
+          />
         </div>
         <div className="split-line w-1 bg-gray-300"></div>
         <div className="right flex flex-1 flex-col bg-red-50">
           <h2 className="px-2 bg-red-100">Drop Target</h2>
-          <ExampleGrid getGridView={getGridViewBuggy}  rows={[]} columns={columns} fields={fields}/>
+          <ExampleGrid
+            getGridView={getGridViewBuggy}
+            rows={[]}
+            columns={columns}
+            fields={fields}
+            onInitGrid={(_) => (targetGrid2.current = _)}
+          />
         </div>
       </div>
-  </>
+    </>
+  );
 }
--- src/views/_components/ExampleGrid.tsx
+++ src/views/_components/ExampleGrid.tsx
@@ -1,67 +1,84 @@
-
 import { useEffect, useRef } from "react";
-import { DataColumn, DataDropMode, DataField, DataValues, GridView, LocalDataProvider, SelectionMode } from "realgrid-2.7.2";
+import {
+  DataColumn,
+  DataDropMode,
+  DataField,
+  DataValues,
+  DragFeedbackStyle,
+  GridView,
+  LocalDataProvider,
+  SelectionMode,
+} from "realgrid-2.7.2";
 
-type Props =  {
-  getGridView: (container: HTMLDivElement) => GridView, 
+type Props = {
+  getGridView: (container: HTMLDivElement) => GridView;
+  onInitGrid?: (grid: GridView) => void;
   columns: DataColumn[];
   fields: DataField[];
   rows: DataValues[];
   movable?: boolean;
   hideRowIndicator?: boolean;
-}
+};
 
 export default function ExampleGrid(props: Props) {
-  const {rows, columns, fields, movable, hideRowIndicator} = props;
+  const { rows, columns, fields, movable, hideRowIndicator } = props;
   const realgridElement = useRef(null);
 
   useEffect(() => {
     const container = realgridElement.current!;
-    const grid =  props.getGridView(container);
-    const provider = grid.getDataSource() as LocalDataProvider;
-    
-    grid.setStateBar({ visible: false });
-    grid.setCheckBar({ visible: false });
-    grid.setFooter({ visible: false });
-    grid.setRowIndicator({ visible: false });
+    const gridView = props.getGridView(container);
+    const provider = gridView.getDataSource() as LocalDataProvider;
+
+    gridView.setStateBar({ visible: false });
+    gridView.setCheckBar({ visible: false });
+    gridView.setFooter({ visible: false });
+    gridView.setRowIndicator({ visible: false });
+
     if (hideRowIndicator) {
-      grid.rowIndicator.visible = false;
+      gridView.rowIndicator.visible = false;
     }
-    
-    grid.setDisplayOptions({
+
+    gridView.setDisplayOptions({
       selectionMode: SelectionMode.SINGLE,
       selectAndImmediateDrag: true,
     });
-    
-    grid.setRowStyleCallback((_, item) => {
-      const checked = item.checked ? "checked " : ""
-      return checked ? "row-selected" : '';
-    })
-    
-    grid.onCurrentRowChanged = (grid, _, rowIdx) => {
-      console.log('onCurrentRowChanged');
+
+    gridView.setRowStyleCallback((_, item) => {
+      const checked = item.checked ? "checked " : "";
+      return checked ? "row-selected" : "";
+    });
+
+    gridView.onCurrentRowChanged = (grid, _, rowIdx) => {
+      console.log("onCurrentRowChanged");
       grid.checkAll(false);
       grid.checkRow(rowIdx, true);
-    }
-    
+    };
+
     provider.setFields(fields);
-    grid.setColumns(columns);
+    gridView.setColumns(columns);
     provider.setRows(rows);
 
     if (movable) {
-      grid.editOptions.movable = true;
-      grid.dataDropOptions.dropMode = DataDropMode.COPY;
-      grid.dataDropOptions.dropOtherElement = true;
+      gridView.editOptions.movable = true;
+      gridView.setDataDropOptions({
+        dropMode: DataDropMode.COPY,
+        dropOtherElement: true,
+      });
+    } else {
+      gridView.setDataDropOptions({
+        // DropTarget 의 피드백 스타일을 Row 로 설정하면 2.8.X에서 동작하지 않음.
+        feedbackStyle: DragFeedbackStyle.ROW,
+      });
     }
+
+    props.onInitGrid?.(gridView);
 
     return () => {
       provider.clearRows();
-      grid.destroy();
+      gridView.destroy();
       provider.destroy();
     };
   }, []);
 
-  return (
-    <div ref={realgridElement} className="flex-1 bg-blue-50"></div>
-  )
+  return <div ref={realgridElement} className="flex-1 bg-blue-50"></div>;
 }
